bbf7c0ca992467da5c5c8cb1f09945cc01507224
AkunoHidai <danchuyennnsu@gmail.com>
2022-11-24 23:02:21 +0700

    Create implement error!

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const contactController = require('./controller/contact.controller');
+const ApiError = require('./api-error');
 
 const app = express();
 
@@ -8,18 +9,29 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json({message : "Hello, world!"})
+    res.json({message : "Hello, world!"});
 });
 // create a route to contact with FGO character
 app.route('/admin/FGO')
     .post(contactController.createFGO)
     .delete(contactController.deleteFGO)
-    .put(contactController.fixFGO)
+    .put(contactController.fixFGO);
 
 // create a route to contact with Princess connect character
 app.route('/admin/princess-connect')
     .post(contactController.createPR)
     .delete(contactController.deletePR)
-    .put(contactController.fixPR)
+    .put(contactController.fixPR);
+
+// create new api handlers
+app.use(function (req, res, next) {
+    return next(new ApiError(404, 'Resource not found!'));
+});
+
+app.use(function (err, req, res, next) {
+    return res.status(err.statusCode || 500).json({
+        message : err.message || 'Internal Server Error'
+    });
+});
 
 module.exports = app;
