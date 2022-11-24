e161daa8168bf4d2b4a167eda109009c46ae5d8c
AkunoHidai <danchuyennnsu@gmail.com>
2022-11-24 22:54:31 +0700

    Add controller to this project

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const contactController = require('./controller/contact.controller');
 
 const app = express();
 
@@ -9,5 +10,16 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.json({message : "Hello, world!"})
 });
+// create a route to contact with FGO character
+app.route('/admin/FGO')
+    .post(contactController.createFGO)
+    .delete(contactController.deleteFGO)
+    .put(contactController.fixFGO)
+
+// create a route to contact with Princess connect character
+app.route('/admin/princess-connect')
+    .post(contactController.createPR)
+    .delete(contactController.deletePR)
+    .put(contactController.fixPR)
 
 module.exports = app;
---
+++ src/controller/contact.controller.js
@@ -0,0 +1,18 @@
+exports.createFGO = function(req, res, next) {
+    return res.send({message : 'install fgo successfully'});
+}
+exports.deleteFGO = function(req, res, next) {
+    return res.send({message : 'delete fgo successfully'});
+}
+exports.fixFGO = function(req, res, next) {
+    return res.send({message : 'fix fgo successfully'});
+}
+exports.createPR = function(req, res, next) {
+    return res.send({message : 'install pr successfully'});
+}
+exports.deletePR = function(req, res, next) {
+    return res.send({message : 'delete pr successfully'});
+}
+exports.fixPR = function(req, res, next) {
+    return res.send({message : 'fix pr successfully'});
+}
